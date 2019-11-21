30e7dbebfeb1d42843d719483bcf2838c32eb7d3
diayoak <diayoak@gmail.com>
2019-11-21 16:52:20 +0900

    programmers

--- programmers/default_useString.py
+++ programmers/default_useString.py
@@ -1,6 +1,6 @@
-rank:	1
-	title:	문자열 다루기 기본
-	link: 	https://programmers.co.kr/learn/courses/30/lessons/12918
+# rank:	1
+# title:	문자열 다루기 기본
+# link: 	https://programmers.co.kr/learn/courses/30/lessons/12918
     
 import re
 
