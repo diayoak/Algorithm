822f8b98ee5123ba3c4810656503283f88674d9f
diayoak <diayoak@gmail.com>
2019-11-25 13:32:41 +0900

    programmers

--- programmers/default_useString.py
+++ programmers/default_useString.py
@@ -2,8 +2,5 @@
 # title:	문자열 다루기 기본
 # link: 	https://programmers.co.kr/learn/courses/30/lessons/12918
     
-import re
-
 def solution(s):
-    reg = re.compile("^(\d{4}|\d{6})$")
-    return bool(reg.match(s))
+    return s.isdigit() and len(s) in (4, 6)
