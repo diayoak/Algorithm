825b63ab1be0bf62607f9fa1e241e8e36a05321d
diayoak <diayoak@gmail.com>
2019-11-27 11:49:41 +0900

    programmers

---
+++ programmers/default_jadenCase.js
@@ -0,0 +1,10 @@
+/*
+	rank:	2
+	title:	JadenCase 문자열 만들기
+
+	link: 	https://programmers.co.kr/learn/courses/30/lessons/12951
+*/
+
+function solution(s) {
+    return s.split(" ").map(str => str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase()).join(" ");
+}
