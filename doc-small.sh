c42659b311b73d925285ab7639f324097c0ee06f
jinny.moon <jinny.moon@kakaoenterprise.com>
2020-08-11 10:33:34 +0900

    temp

--- doc-small.sh
+++
@@ -1,7 +0,0 @@
-START_SECOND=$(date +%s)
-START_MILLISECOND=$(date +%n)
-./convert.sh ./TestFile/doc:docx/doc-small.docx ./outputs/doc/ image
-END_SECOND=$(date +%s)
-END_MILLISECOND=$(date +%n)
-
-echo "doc-small.docx : $(($END_SECOND - $START_SECOND)).$(($END_MILLISECOND - $START_MILLISECOND)) seconds"
